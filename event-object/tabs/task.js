c6d873c96aa242ca5a653ce45b350c07d923c241
Sergey Kievskiy <knhel7@mail.ru>
2023-01-23 08:44:13 +0300

    tabs edited

---
+++ event-object/tabs/task.js
@@ -0,0 +1,60 @@
+// Домашнее задание к занятию 1.3 «Объект события».
+// Задание 1. Выпадающие списки
+
+const tabsClasses = {
+    containerClass: 'tabs',
+    navigation: 'tab__navigation',
+    navigationItem: 'tab',
+    tabActivation: 'tab_active',
+    contents: 'tab__contents',
+    contentItem: 'tab__content',
+    contentActivation: 'tab__content_active'
+}
+let myHandlers = setEventHandlers();
+let container = (id) => document.getElementById(id);
+
+function setTabsEventHandlers() {
+    let handlers = {};
+    /*
+      TODO:
+     */
+    return handlers;
+}
+
+function setEventHandlers() {
+    let handlers = {};
+    handlers.mouse.tabs = setTabsEventHandlers();
+    /*
+      TODO:
+     */
+    return handlers;
+}
+
+function startHandlers() {
+    /*
+      TODO:
+     */
+}
+
+function stopHandlers() {
+    /*
+      TODO:
+     */
+}
+
+function start() {
+    startHandlers();
+    /*
+      TODO:
+     */
+}
+
+function stop() {
+    stopHandlers();
+    /*
+      TODO:
+     */
+}
+
+
+start();
