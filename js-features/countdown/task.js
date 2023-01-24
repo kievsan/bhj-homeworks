c9da174be7810c8f66ea5a94e375245bf2b58f94
Sergey Kievskiy <knhel7@mail.ru>
2023-01-24 15:08:00 +0300

    countdown edited

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -5,39 +5,101 @@
 
 
 class Timer {
-    constructor(startTime = 0, timerID = 'timer') {
-        this.timerElement = document.getElementById(timerID);
-        this.intervalID = 0;
+    static clock = {
+        secondsToHhMmSc: (sec) => {
+            let time = {Hh: 0, Mm: 0, Sc: 0};
+            time.Mm = ~~(sec/60);
+            time.Ss = `0${sec % 60}`.slice(-2);
+            time.Hh = `0${~~(time.Mm/60)}`.slice(-2);
+            time.Mm = `0${time.Mm % 60}`.slice(-2);
+            return `${time.Hh}:${time.Mm}:${time.Ss}`;
+        }
     }
 
-    setClockTime(newTime) { this.timerElement.textContent = Math.abs(newTime) }
-    getClockTime() { return parseInt(this.timerElement.textContent) }
+    constructor(timerID = 'timer') {
+        this.timerElement = document.getElementById(timerID);
+    }
 
-    timerStop = () => {
-        clearInterval(this.intervalID);
-        this.setClockTime(0);
+    simpleTime = (time) => Math.abs(parseInt(time));
+    advancedTime = (time) => Timer.clock.secondsToHhMmSc(time);
+
+    setClockTime(newTime, isAdvanced = false) {  // Почему таймер скачет то выше, то ниже строчкой не один раз???
+        this.timerElement.textContent = isAdvanced ? this.advancedTime(newTime) : newTime;
+    }
+
+    getClockTime() { return this.timerElement.textContent }
+
+    timerStop = (intervalID) => {
+        clearInterval(intervalID);
         alert('Вы победили в конкурсе');
     }
 
-    startSimpleTimer(startDelay = 4, tickDelay = 1000) {
+    startSimpleTimer(startTime = 0, startDelay = 4, tickDelay = 1000) {
+        startTime = startTime ? this.simpleTime(startTime) : this.getClockTime();
+        this.setClockTime(startTime);
         let currentDelay = startDelay;
         let start = new Date().getTime();
 
-        this.intervalID = setInterval(() => {
+        let intervalID = setInterval(() => {
             if (new Date().getTime() - start < currentDelay) {
                 currentDelay = tickDelay + start - new Date().getTime();
             } else {
-                let time = this.timerElement.textContent--;
-                if (!time) {
-                    this.timerStop();
+                if (!this.timerElement.textContent--) {
+                    ++this.timerElement.textContent;
+                    this.timerStop(intervalID);
+                    return;
                 }
                 currentDelay = tickDelay;
                 start = new Date().getTime();
             }
         }, currentDelay);
     }
+
+    startSimpleTimer2(startTime = 0, startDelay = 4, tickDelay = 1000) {
+        startTime = startTime ? this.simpleTime(startTime) : this.getClockTime();
+        this.setClockTime(startTime);
+        let currentDelay = startDelay;
+        let intervalID;
+
+        let countdown = function () {
+            if (!this.timerElement.textContent--) {
+                ++this.timerElement.textContent;
+                this.timerStop(intervalID);
+                return;
+            }
+            currentDelay = tickDelay;
+            intervalID = setTimeout(countdown, currentDelay);
+        }.bind(this);
+
+        countdown();
+    }
+
+    startAdvancedTimer(startTime = 0, startDelay = 4, tickDelay = 1000) {
+        startTime = startTime ? this.simpleTime(startTime) : this.getClockTime();
+        this.setClockTime(startTime, true);
+        let currentTime = startTime;
+        let currentDelay = startDelay;
+        let intervalID;
+        
+        let countdown = function () {
+            let time = currentTime--;
+            this.setClockTime(currentTime, true);
+            if (!time) {
+                this.timerElement.textContent = '00:00:00';
+                this.timerStop(intervalID);
+                return;
+            }
+            currentDelay = tickDelay;
+            intervalID = setTimeout(countdown, currentDelay);
+        }.bind(this);
+
+        countdown();
+    }
 }
 
 
 let timer = new Timer();
-timer.startSimpleTimer();
+// timer.startSimpleTimer();
+// timer.startSimpleTimer2(100);
+// timer.startAdvancedTimer(66);
+timer.startAdvancedTimer();
