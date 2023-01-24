f2723a597c0026c9e197cebb2442cb12608eee38
Sergey Kievskiy <knhel7@mail.ru>
2023-01-24 20:26:45 +0300

    mole-game edited

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,19 +4,31 @@
 let mainContainer = document.querySelector('main.content');
 let holesContainer = mainContainer.querySelector('.hole-game');
 let statusesContainer = document.getElementById('status');
+let activationClass = 'hole_has-mole'
+let deadCounter = document.getElementById('dead');
+let lossCounter = document.getElementById('lost');
 let gameHandlers = setEventHandlers();
 
 function setEventHandlers() {
     let handlers = {holes: {}};
 
-    handlers.holes.click = function (event) {
+    let getActiveHole = () => holesContainer.querySelector(`.${activationClass}`);
+    let isActiveHole = (holeID) => document.getElementById(holeID).classList.contains(activationClass);
 
+    handlers.holes.click = function (event) {
+        if (event.target.classList.contains('hole')) {
+            if (isActiveHole(event.target.id)) {
+                ++deadCounter.textContent;
+            } else {
+                ++lossCounter.textContent;
+            }
+        }
     }
 
     return handlers;
 }
 
-function startHandlers() { holesContainer.addEventListener('click', gameHandlers.click, {passive: true}); }
+function startHandlers() { holesContainer.addEventListener('click', gameHandlers.click); }
 
 function stopHandlers() { cookieHTML.removeEventListener('click', gameHandlers.click); }
 
