0b62babfc34e317a0876a0eb380ff5c386ee405d
Sergey Kievskiy <knhel7@mail.ru>
2023-01-28 22:20:06 +0300

    chat edited

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -11,9 +11,47 @@ function setChatEventHandlers() {
         chatContainer = document.body.querySelector(`.${chatContainerClass}`),
         chatActivationClass = 'chat-widget_active',
         isActiveChat = () => chatContainer.classList.contains(chatActivationClass),
-        inputChatContainer = document.getElementById(`chat-widget__input`),
-        messagesContainer = chatContainer.querySelector( '.chat-widget__messages-container' ),
-        messages = chatContainer.querySelector( '.chat-widget__messages' );
+        inputChatContainer = document.getElementById(`chat-widget__input`);
+
+    const messagesContainer = chatContainer.querySelector( '.chat-widget__messages-container' ),
+        messages = chatContainer.querySelector( '.chat-widget__messages' ),
+        scrollDownMessages = () => messagesContainer.scrollTop = messagesContainer.scrollHeight,
+        getRandBotMsg = () => {
+            const phrases = [
+                    'Hi!', 'By!', 'Ok', 'I don\'t care...', 'I don\'t understand...'
+                ],
+                index = Math.floor(Math.random() * phrases.length);
+            return phrases[index];
+        },
+        hasStopText = () => {
+            for (let stopText of ['fuck']) {
+                if (myHandlers.chat.currentMassage.includes(stopText.toLowerCase())) {
+                    return true; }}
+            return false;
+        },
+        getTime = () => new Date().toLocaleTimeString('ru', {hour: "2-digit", minute: "2-digit"}),
+        sendUserMsg = () => {
+            messages.innerHTML += `
+                  <div class="message message_client">
+                    <div class="message__time">${getTime()}</div>
+                    <div class="message__text">
+                      ${myHandlers.chat.currentMassage}
+                    </div>
+                  </div>
+                `;
+            clearInterval(myHandlers.chat.intervalID);
+        },
+        sendBotMsg = () => {
+            messages.innerHTML += `
+                  <div class="message">
+                    <div class="message__time">${getTime()}</div>
+                    <div class="message__text">
+                      ${getRandBotMsg()}
+                    </div>
+                  </div>
+                `;
+            clearInterval(myHandlers.chat.intervalID);
+        };
 
     inputChatContainer.setAttribute('minlength', '1');
     inputChatContainer.setAttribute('maxlength', '50');
@@ -32,55 +70,24 @@ function setChatEventHandlers() {
         }
     }
 
-    handlers.input = (event) => handlers.currentMassage = event.target.value.trim();
+    handlers.input = (event) => myHandlers.chat.currentMassage = event.target.value.trim();
 
     handlers.keydown = (event) => {
-        const isChatInput = () => event.target.closest(`input.${'chat-widget__input'}`),
-            hasStopText = () => {
-                for (let stopText of ['fuck']) {
-                    if (myHandlers.chat.currentMassage.includes(stopText.toLowerCase())) {
-                        return true; }
-                }
-                return false;
-            },
-            getRandBotMsg = () => {
-                const phrases = [
-                        'Hi!', 'By!', 'Ok', 'I don\'t care...', 'I don\'t understand...'
-                    ],
-                    index = Math.floor(Math.random() * phrases.length);
-                return phrases[index];
-            },
-            getTime = () => new Date().toLocaleTimeString('ru', {hour: "2-digit", minute: "2-digit"});
+        const isChatInput = () => event.target.closest(`input.${'chat-widget__input'}`);
 
         if (event.keyCode === 13 && isChatInput()) {
             document.activeElement.blur();
 
             if (inputChatContainer.checkValidity() && myHandlers.chat.currentMassage && !hasStopText()) {
-                // Клиент
-                messages.innerHTML += `
-                  <div class="message message_client">
-                    <div class="message__time">${getTime()}</div>
-                    <div class="message__text">
-                      ${myHandlers.chat.currentMassage}
-                    </div>
-                  </div>
-                `;
-                // Бот
-                messages.innerHTML += `
-                  <div class="message">
-                    <div class="message__time">${getTime()}</div>
-                    <div class="message__text">
-                      ${getRandBotMsg()}
-                    </div>
-                  </div>
-                `;
-                // Прокрутка вниз
-                let intervalID = setTimeout(() => {
-                    messagesContainer.scrollTop = messagesContainer.scrollHeight;
-                }, 100);
+                sendUserMsg();
+                myHandlers.chat.intervalID = setTimeout(scrollDownMessages, 10);
+                myHandlers.chat.intervalID = setTimeout(() => {
+                    sendBotMsg();
+                    myHandlers.chat.intervalID = setTimeout(scrollDownMessages, 10);
+                }, 300);
             }
             event.target.value = '';
-            handlers.currentMassage = '';
+            myHandlers.chat.currentMassage = '';
 
             event.target.focus();
         }
